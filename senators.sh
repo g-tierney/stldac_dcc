9a4e5823ba34a9ec1641485d1d144073d40792a6
Graham Tierney <gt83@dcc-slogin-01.oit.duke.edu>
2020-07-06 14:08:36 -0400

    initialize

---
+++ senators.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=volfovskylab
+#SBATCH -p volfovskylab,statdept-low,herringlab-low,common
+#SBATCH -N1
+#SBATCH -c1
+#SBATCH --mem=200G
+
+module load R
+Rscript scripts/vb_senTweets_execute.R
