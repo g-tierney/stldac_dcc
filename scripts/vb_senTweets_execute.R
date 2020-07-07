40407f3cdcf6674d736d938a373b618d3517d6dd
Graham Tierney <gtierney2@gmail.com>
2020-07-07 10:24:12 -0400

    Update vb_senTweets_execute.R

--- scripts/vb_senTweets_execute.R
+++ scripts/vb_senTweets_execute.R
@@ -15,7 +15,7 @@ load('senatorTweet_data/rinputs.Rdata')
 dw_mat <- convert(senTweets116_2020.dfm_trimmed,to="matrix")
 rm(senTweets116_2020.dfm_trimmed)
 
-n <- nrow(dw_mat) #9000 
+n <- 3000 #nrow(dw_mat) # 
 dw_mat <- dw_mat[1:n,]
 senators <- senators[1:n]
 nCores <- min(1,round(parallel::detectCores()/2),length(unique(senators)))
@@ -23,7 +23,7 @@ nCores <- min(1,round(parallel::detectCores()/2),length(unique(senators)))
 print(str_c(n," Tweets"))
 print(str_c("Using ",nCores," cores."))
 
-x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 200,n.cores=nCores)
+x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 2,n.cores=nCores)
 
 
 saveRDS(x,file = "senatorTweet_data/vb_4C_30T_200Max_1Core.rds")
