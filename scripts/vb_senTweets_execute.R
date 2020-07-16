7e25948f255a23fa4ab7413587e11ab6a6a926cd
g-tierney <gtierney2@gmail.com>
2020-07-16 11:45:43 -0400

    Add small sample of tweets option for testing

--- scripts/vb_senTweets_execute.R
+++ scripts/vb_senTweets_execute.R
@@ -13,15 +13,23 @@ source("scripts/vb_implementation.R")
 
 load('senatorTweet_data/rinputs.Rdata')
 dw_mat <- convert(senTweets116_2020.dfm_trimmed,to="matrix")
-rm(senTweets116_2020.dfm_trimmed)
 
-n <-   nrow(dw_mat) # 3000
-dw_mat <- dw_mat[1:n,]
-senators <- senators[1:n]
-nCores <- min(4,round(parallel::detectCores()),length(unique(senators)))
 
-print(str_c(n," Tweets"))
+set.seed(196)
+small_n <- senTweets116_2020.dfm_trimmed@docvars %>% 
+  group_by(screen_name) %>% 
+  sample_n(4) %>% 
+  select(docname_)
+
+n <-   which(senTweets116_2020.dfm_trimmed@docvars$docname_ %in% small_n$docname_) #1:nrow(dw_mat) # 
+dw_mat <- dw_mat[n,]
+senators <- senators[n]
+nCores <- min(1,round(parallel::detectCores()),length(unique(senators)))
+
+print(str_c(length(n)," Tweets"))
 print(str_c("Using ",nCores," cores."))
+
+#rm(senTweets116_2020.dfm_trimmed)
 
 x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 3,n.cores=nCores)
 
