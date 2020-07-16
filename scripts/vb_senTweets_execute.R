a14a3a6cc6c80638ff32f135b2f2e1f6dd015780
g-tierney <gtierney2@gmail.com>
2020-07-16 12:38:30 -0400

    Core testing

--- scripts/vb_senTweets_execute.R
+++ scripts/vb_senTweets_execute.R
@@ -21,17 +21,23 @@ small_n <- senTweets116_2020.dfm_trimmed@docvars %>%
   sample_n(4) %>% 
   select(docname_)
 
-n <-   1:nrow(dw_mat) # which(senTweets116_2020.dfm_trimmed@docvars$docname_ %in% small_n$docname_) #
+n <-   which(senTweets116_2020.dfm_trimmed@docvars$docname_ %in% small_n$docname_) #1:nrow(dw_mat) # 
 dw_mat <- dw_mat[n,]
 senators <- senators[n]
-nCores <- min(Inf,round(parallel::detectCores()/1),length(unique(senators)))
+nCores <- min(1,round(parallel::detectCores()/1),length(unique(senators)))
 
 print(str_c(length(n)," Tweets"))
 print(str_c("Using ",nCores," cores."))
 
 rm(senTweets116_2020.dfm_trimmed,small_n)
 
-x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 2,n.cores=nCores)
+x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 2,n.cores=1)
+rm(x)
+gc()
+x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 2,n.cores=4)
+rm(x)
+gc()
+x <- stldac_vb(users=senators,dw=dw_mat,nT = 10,nC = 4,tol = .01,seed = 1,maxiter = 2,n.cores=parallel::detectCores())
 
 gc()
 
