6ecefe0de252a740b6383fb12c1df2c5f5c67ad5
g-tierney <gtierney2@gmail.com>
2020-08-04 16:00:49 -0400

    Update gibbs_execute_senators.sh

--- gibbs_execute_senators.sh
+++ gibbs_execute_senators.sh
@@ -3,7 +3,7 @@
 #SBATCH -p volfovskylab,statdept-low,herringlab-low,common
 #SBATCH -N1
 #SBATCH -c1
-#SBATCH --mem=100G
+#SBATCH --mem=50G
 
 module load R
 Rscript scripts/gibbs_senTweets_execute.R
