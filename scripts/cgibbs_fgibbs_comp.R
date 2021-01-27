ee9dc344d0f4909ed83e8b83353858b4967c6ce0
g-tierney <gtierney2@gmail.com>
2021-01-27 16:18:32 -0500

    Scale up simulation

--- scripts/cgibbs_fgibbs_comp.R
+++ scripts/cgibbs_fgibbs_comp.R
@@ -22,7 +22,7 @@ dat <- make_data(nW = 4000,nT = 10,topic_low = 1,topic_high = 10,nC = 3,alpha_tr
 fgibbs <- full_gibbs_1topic(alpha = 1,eta = .1,nu = 1,
                             users = dat$users,dw = dat$dw,
                             nT = dat$nT,nC = dat$nC,
-                            niter = 20,nClusterIter = 50,
+                            niter = 2000,nClusterIter = 50,
                             seed = seed.sim,
                             mu_scale = 0,sigma_scale = 100)
 #run collapsed gibbs
@@ -31,7 +31,7 @@ cgibbs <- collapsed_gibbs_1topic_clusters(alpha = 1,eta = .1,nu = 1,
                                           nT = dat$nT,nC = dat$nC,
                                           niter = 5,
                                           seed = seed.sim,mcmc_update = T,
-                                          nClusterIter = 50,
+                                          nClusterIter = 500,
                                           mu_scale = 0,sigma_scale = 100,
                                           prop_scale_center = 100,alphag_sample_method = "componentwise",
                                           print_clusters = T)
