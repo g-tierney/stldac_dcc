3d8998ff1f3525942826e986230f3137b67e0780
g-tierney <gtierney2@gmail.com>
2021-01-29 11:07:47 -0500

    Update cgibbs_fgibbs_comp.R
    Smaller simulation started at truth

--- scripts/cgibbs_fgibbs_comp.R
+++ scripts/cgibbs_fgibbs_comp.R
@@ -22,30 +22,30 @@ dat <- make_data(nW = 4000,nT = 10,topic_low = 1,topic_high = 10,nC = 3,alpha_tr
 fgibbs <- full_gibbs_1topic(alpha = 1,eta = .1,nu = 1,
                             users = dat$users,dw = dat$dw,
                             nT = dat$nT,nC = dat$nC,
-                            niter = 2000,nClusterIter = 50,
+                            niter = 200,nClusterIter = 50,
                             seed = seed.sim,
                             mu_scale = 0,sigma_scale = 100)
 #run collapsed gibbs
 cgibbs <- collapsed_gibbs_1topic_clusters(alpha = 1,eta = .1,nu = 1,
                                           users = dat$users,dw = dat$dw,
                                           nT = dat$nT,nC = dat$nC,
-                                          niter = 5,
+                                          niter = 50,
                                           seed = seed.sim,mcmc_update = T,
-                                          nClusterIter = 500,
+                                          nClusterIter = 50,
                                           mu_scale = 0,sigma_scale = 100,
                                           prop_scale_center = 100,alphag_sample_method = "componentwise",
                                           print_clusters = T)
 
 print("full gibbs topic recovery")
-fgibbs$ta %>% results_freq_table() %>% table(dat$ta_true)
-fgibbs$ta %>% results_freq_table() %>% table(dat$ta_true) %>% nmi
+fgibbs$ta %>% results_freq_table() %>% apply(1,which.max) %>% table(dat$ta_true)
+fgibbs$ta %>% results_freq_table() %>% apply(1,which.max) %>% table(dat$ta_true) %>% nmi
 
 print("collapsed gibbs topic recovery")
-cgibbs$ta %>% results_freq_table() %>% table(dat$ta_true)
-cgibbs$ta %>% results_freq_table() %>% table(dat$ta_true) %>% nmi
+cgibbs$ta %>% results_freq_table() %>% apply(1,which.max) %>% table(dat$ta_true)
+cgibbs$ta %>% results_freq_table() %>% apply(1,which.max) %>% table(dat$ta_true) %>% nmi
 
 
 
 if(!dir.exists("output/cgibbs_fgibbs_comparison")) dir.create("output/cgibbs_fgibbs_comparison")
 
-saveRDS(list(fgibbs = fgibbs,cgibbs = cgibbs),file = "output/cgibbs_fgibbs_comparison/gibbs_results.rds")
+#saveRDS(list(fgibbs = fgibbs,cgibbs = cgibbs),file = "output/cgibbs_fgibbs_comparison/gibbs_results.rds")
