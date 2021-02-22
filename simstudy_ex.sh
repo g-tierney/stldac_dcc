75284e8fa35b96df416603d3a7b00f60b6cfdacb
g-tierney <gtierney2@gmail.com>
2021-02-22 12:48:54 -0500

    Update simstudy_ex.sh

--- simstudy_ex.sh
+++ simstudy_ex.sh
@@ -6,7 +6,9 @@
 #SBATCH --mem=50G
 #SBATCH -o log_simstudy_ex.txt
 #SBATCH --array=1-2
+#SBATCH --mail-user=gt83@duke.edu
+#SBATCH --mail-type=END,FAIL
 
 module load R
-R CMD BATCH --no-restore --no-save vb_gibbs_simstudy.R rlog_simstudy$SLURM_ARRAY_TASK_ID
+R CMD BATCH --no-restore --no-save scripts/vb_gibbs_simstudy.R rlog_simstudy$SLURM_ARRAY_TASK_ID.Rout
 
