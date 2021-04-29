b874a2bbafcb8a08d227bca6cce7057d878c3786
g-tierney <gtierney2@gmail.com>
2021-04-29 19:49:22 -0400

    Update prep_senator_data.R

--- scripts/prep_senator_data.R
+++ scripts/prep_senator_data.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 
 library(tidyverse)
 library(quanteda)
@@ -11,13 +12,6 @@ senTweets116 <- senTweets116.full %>%
 senDemos <- openxlsx::read.xlsx("senatorTweet_data/116_Senator_Twitter_Handles.xlsx") %>% 
   filter(!str_detect(senator_name,"Isakson"))
 
-# senTweets116 %>% 
-#   filter(date >= as.Date("2020-01-01") & date <= as.Date("2020-05-31"),!str_detect(screen_name,"isakson")) %>% 
-#   group_by(screen_name) %>% 
-#   summarise(n=n(),
-#             first=min(date),
-#             last=max(date)) %>% View
-
 senTweets116_2020 <- senTweets116 %>% 
   filter(date >= as.Date("2020-01-01") & date <= as.Date("2020-05-31"),!str_detect(screen_name,"isakson")) 
 
@@ -25,7 +19,9 @@ senTweets116_2020$text_clean <-
   str_replace_all(senTweets116_2020$text,"https?://([a-z\\.]*)/([:alnum:]*)","hyperlink") #remove URLs
 
 
-senTweets116_2020.dfm <- dfm(senTweets116_2020$text_clean,tolower = T,remove = c(stopwords(source = "smart"),"hyperlink","amp"),remove_punct = T)
+senTweets116_2020.dfm <- dfm(senTweets116_2020$text_clean,tolower = T,
+                             remove = c(stopwords(source = "smart"),"hyperlink","amp"),
+                             remove_punct = T)
 docvars(senTweets116_2020.dfm) <- senTweets116_2020 %>% ungroup()
 
 senTweets116_2020.dfm %>% colSums() %>% sort(decreasing = T) %>% {.[1:10]}
@@ -43,11 +39,11 @@ senTweets116_2020.dfm_trimmed
 
 dw_mat <- senTweets116_2020.dfm_trimmed %>% 
   convert(to="matrix")
-senators <- senTweets_trimmed.dfm@docvars$screen_name
+senators <- senTweets116_2020.dfm_trimmed@docvars$screen_name
 
 #seed set for creating a small dataset for testing (x from each senator)
 set.seed(196)
-small_n <- senTweets_trimmed.dfm@docvars %>% 
+small_n <- senTweets116_2020.dfm_trimmed@docvars %>% 
   group_by(screen_name) %>% 
   sample_n(pmin(4,n())) %>% 
   select(docname_)
@@ -59,21 +55,17 @@ dw_mat <- dw_mat[n,]
 senators <- senators[n]
 
 print(str_c(length(n)," Tweets"))
-print(str_c("Using ",nCores," cores."))
 
 #rm(senTweets116_2020.dfm_trimmed,small_n)
 
 #extract test set
 set.seed(196)
-test <- senTweets_trimmed.dfm@docvars %>% 
+test <- senTweets116_2020.dfm_trimmed@docvars %>% 
   group_by(screen_name) %>% 
   slice_sample(prop = 0.1) %>% 
-  {senTweets_trimmed.dfm@docvars$docname_ %in% .$docname_} %>% 
+  {senTweets116_2020.dfm_trimmed@docvars$docname_ %in% .$docname_} %>% 
   which
 train <- c(1:nrow(dw_mat))[!(1:nrow(dw_mat)) %in% test]
 set.seed(NULL)
-
-dw_train <- dw_mat[train,]
-dw_test <- dw_mat[test,]
 
 save(dw_mat,test,train,senators,file = "temp/dw_train.RData")
