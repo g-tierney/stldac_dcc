a9eed0a712971219758f223e799434de18bf9078
g-tierney <gtierney2@gmail.com>
2021-04-29 20:04:25 -0400

    Update vb_senators_choose_G.R
    Remove unnecessary package

--- scripts/vb_senators_choose_G.R
+++ scripts/vb_senators_choose_G.R
@@ -3,8 +3,6 @@ rm(list=ls())
 print(parallel::detectCores())
 
 library(tidyverse)
-library(quanteda)
-library(stopwords)
 
 source("scripts/setup.R")
 source("scripts/helper_functions.R")
@@ -18,7 +16,7 @@ if(Sys.getenv('SLURM_ARRAY_TASK_ID') != ""){
 } else slurm_id <- 1
 
 #set parameters
-maxiter <- 1000; nC <- 2*slurm_id; nT <- 30; seed <- 196
+maxiter <- 3; nC <- 2*slurm_id; nT <- 30; seed <- 196
 nCores <- min(1,round(parallel::detectCores()/1),length(unique(senators)))
 
 x <- stldac_vb(users=senators[train],dw=dw_mat[train,],nT = nT,nC = nC,tol = .01,seed = seed,maxiter = 2,n.cores=nCores)
