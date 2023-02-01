f435bc9709f483bdaa1f65792f20543b882abb8f
Graham Tierney <gtierney2@gmail.com>
2023-02-01 15:47:54 -0500

    Update vb_execute_senators_JanMay.sh

--- vb_execute_senators_JanMay.sh
+++ vb_execute_senators_JanMay.sh
@@ -3,7 +3,7 @@
 #SBATCH -p volfovskylab,statdept-low,herringlab-low,common
 #SBATCH -N1
 #SBATCH -c1
-#SBATCH --mem=50G
+#SBATCH --mem=100G
 #SBATCH -o logs/log_vb_SenTweets_JanMay_%A.txt
 #SBATCH --array=1-16
 #SBATCH --mail-user=gt83@duke.edu
