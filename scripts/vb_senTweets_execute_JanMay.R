c7ef300fc16c75f19cb1e41ad4021a87b6d28d0b
Graham Tierney <gtierney2@gmail.com>
2023-02-01 11:58:56 -0500

    Update vb_senTweets_execute_JanMay.R

--- scripts/vb_senTweets_execute_JanMay.R
+++ scripts/vb_senTweets_execute_JanMay.R
@@ -11,21 +11,19 @@ source("scripts/helper_functions.R")
 source("scripts/vb_stldac_functions.R")
 source("scripts/vb_implementation.R")
 
-load('senatorTweet_data/senatorTweet_dfms.Rdata')
-senTweets_trimmed.dfm <- senTweets_trimmed.dfm %>% 
-  dfm_subset(created_at < "2020-05-31")
-dw_mat <- convert(senTweets_trimmed.dfm,to="matrix")
-senators <- senTweets_trimmed.dfm@docvars$screen_name
+load('senatorTweet_data/rinputs.Rdata')
+dw_mat <- convert(senTweets116_2020.dfm_trimmed,to="matrix")
+senators <- senTweets116_2020.dfm_trimmed@docvars$screen_name
 
 #seed set for creating a small dataset for testing (x from each senator)
 set.seed(196)
-small_n <- senTweets_trimmed.dfm@docvars %>% 
+small_n <- senTweets116_2020.dfm_trimmed@docvars %>% 
   group_by(screen_name) %>% 
   sample_n(pmin(4,n())) %>% 
   select(docname_)
 
 #switch n values to choose sample or full data
-n <-  which(senTweets_trimmed.dfm@docvars$docname_ %in% small_n$docname_) # 1:nrow(dw_mat) # 
+n <-   1:nrow(dw_mat) # which(senTweets116_2020.dfm_trimmed@docvars$docname_ %in% small_n$docname_) #
 dw_mat <- dw_mat[n,]
 senators <- senators[n]
 nCores <- min(round(parallel::detectCores()/1),length(unique(senators))/4)
@@ -33,7 +31,7 @@ nCores <- min(round(parallel::detectCores()/1),length(unique(senators))/4)
 print(str_c(length(n)," Tweets"))
 print(str_c("Using ",nCores," cores."))
 
-#rm(senTweets116_2020.dfm_trimmed,small_n)
+rm(senTweets116_2020.dfm_trimmed,small_n)
 
 #set parameters
 nC_vec <- c(2,4,6,8)
@@ -52,4 +50,4 @@ maxiter <- 1000;  seed <- 196
 x <- stldac_vb(users=senators,dw=dw_mat,nT = nT,nC = nC,tol = .01,seed = seed,maxiter = 1000,n.cores=nCores)
 gc()
 
-saveRDS(x,file = str_c("work/gt83/stlda_dcc/output/vb_",nC,"C_",nT,"T_",maxiter,"Max_",nCores,"Core_",seed,"seed_JanMay.rds"))
+saveRDS(x,file = str_c("/work/gt83/stldac_dcc/output/vb_",nC,"C_",nT,"T_",maxiter,"Max_",nCores,"Core_",seed,"seed_JanMay.rds"))
